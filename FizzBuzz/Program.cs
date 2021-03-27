1d9eba40e5bb67c1aa3ab007bb08b48bb036b2ca
wallrusi <lugodwaleed@gmail.com>
2021-03-27 22:29:04 +0800

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output;
+            for (int i = 1; i <= 100; i++)
+            {
+                output = "";
+
+
+                if (i % 3 == 0)
+                {
+                    output += "Fizz";
+                }
+
+                if (i % 5 == 0)
+                {
+                    output += "Buzz";
+                }
+
+
+                if (output == "")
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.WriteLine(output);
+                }
+            }
+        }
+    }
+}
